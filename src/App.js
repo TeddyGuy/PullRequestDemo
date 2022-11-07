b5a1737af499f52caca30fdd4a40b0009a2dce3d
TeddyGuy <mediteddyguy@gmail.com>
2022-11-07 13:54:39 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+
+import { Offcanvas } from 'bootstrap';
+import { Ratio } from 'react-bootstrap';
+import Table from 'react-bootstrap';
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [users, setU] = useState([]);
+
+  const c = () => {
+    const newU = [...users]
+    newU.push({email: email, password: password})
+    setU(newU)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Container fluid>
+      <Row>
+        <Col xs={6}>
+          <Form.Group>
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="joe@biden.us" onChange={e => setEmail(e.target.value)}/>
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={6}>
+          <Form.Group>
+            <Form.Label>Passord</Form.Label>
+            <Form.Control type="password" onChange={e => setPassword(e.target.value)}/>
+          </Form.Group>
+        </Col>
+      </Row>
+      <Button variant="primary" onClick={c} className={"my-3"}>
+        Create
+      </Button>
+      {
+        users.map((user, index) => <Card key={index} style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Title>{user.email}</Card.Title>
+            <Card.Text>
+              {user.password}
+            </Card.Text>
+          </Card.Body>
+        </Card>)
+      }
+    </Container>
+    </>
   );
 }
 
